6508786d65401bc1876bc9f64f05e3524748387c
Caleb Norton <n0603919@outlook.com>
2024-07-08 12:14:56 -0500

    release 0.63.8-beta

--- src/services/build.js
+++ src/services/build.js
@@ -1,1 +1,1 @@
-module.exports = { buildDate:"", buildRevision: "82a437f2a83fc10299f3205713f7dcb04e2d047c" };
+module.exports = { buildDate:"2024-07-08T12:14:56-05:00", buildRevision: "a1d754aa3c54a0fde68f37f36c16fccace57599e" };
